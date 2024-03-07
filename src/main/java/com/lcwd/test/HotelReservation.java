3cde94d0faa9d7deee7c2d7934ee1e094added40
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-07 15:33:56 +0530

    find cheapest hotel and total rate

--- src/main/java/com/lcwd/test/HotelReservation.java
+++ src/main/java/com/lcwd/test/HotelReservation.java
@@ -1,5 +1,6 @@
 package com.lcwd.test;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,14 +20,49 @@ public class HotelReservation {
             }
         }
     }
-    public int getRates(String hotelName, String customerType){
+    public int getTotalRateForDates(String hotelName, LocalDate startDate, LocalDate endDate, String customerType) {
         Hotel hotel = hotels.get(hotelName);
-        if(hotel != null){
-            return hotel.getRates(customerType);
+        if (hotel != null) {
+            int totalRate = 0;
+            LocalDate date = startDate;
+            while (!date.isAfter(endDate)) {
+                totalRate += hotel.getRates(customerType);
+                date = date.plusDays(1);
+            }
+            return totalRate;
         }
-        return 0;
+        return 0; // or throw exception
+    }
+    public String findCheapestHotel(LocalDate startDate, LocalDate endDate, String customerType) {
+        int minTotalRate = Integer.MAX_VALUE;
+        String cheapestHotel = null;
+
+        for (Map.Entry<String, Hotel> entry : hotels.entrySet()) {
+            int totalRate = getTotalRateForDates(entry.getKey(), startDate, endDate, customerType);
+            if (totalRate < minTotalRate) {
+                minTotalRate = totalRate;
+                cheapestHotel = entry.getKey();
+            } else if (totalRate == minTotalRate && entry.getKey().compareTo(cheapestHotel) < 0) {
+                // If rates are the same, select the hotel with the lexicographically smaller name
+                cheapestHotel = entry.getKey();
+            }
+        }
+
+        return cheapestHotel;
     }
     public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Reservation Program");
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.setHotels("Lakewood");
+        hotelReservation.setRates("Lakewood", Map.of("regular", 110));
+        hotelReservation.setHotels("Bridgewood");
+        hotelReservation.setRates("Bridgewood", Map.of("regular", 160));
+        hotelReservation.setHotels("Ridgewood");
+        hotelReservation.setRates("Ridgewood", Map.of("regular", 220));
+        LocalDate startDate = LocalDate.parse("2020-09-10");
+        LocalDate endDate = LocalDate.parse("2020-09-12");
+        String customerType = "regular";
+        String cheapHotel = hotelReservation.findCheapestHotel(startDate,endDate, customerType);
+        int totalRate = hotelReservation.getTotalRateForDates(cheapHotel, startDate, endDate, customerType);
+        System.out.println("Cheapest Hotel: "+cheapHotel+" Total Rates: "+totalRate);
     }
 }
